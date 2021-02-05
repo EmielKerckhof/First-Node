bb331862bb9b5414384e3b7f54f71df5640d4ab7
DVibes <35845628+DVibes@users.noreply.github.com>
2021-02-05 12:57:16 +0100

    extra wijzigingen

--- Groep.js
+++ Groep.js
@@ -1,8 +1,14 @@
 function verdeelInGroep()
-
 {
-    var group = input("Wat is je geboortedatum?");
-    console.log("Je zit in groep " + group);
+/*     var date = prompt("Wat is je geboortedatum?")
+    var tussenDatum = date % 7;
+
+    console.log("Je zit in groep " + tussenDatum) */
+
+
+
+     var group = date % 7;
+    console.log("Je zit in groep " + group); 
 }
 
 verdeelInGroep();
